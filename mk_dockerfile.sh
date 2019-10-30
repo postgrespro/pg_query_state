02049e10afa38b1807ad830fb887a5f5199c4391
Alexey Kondratov <kondratov.aleksey@gmail.com>
2019-10-30 12:46:38 +0300

    Pass USE_TPCDS env variable to Docker container

--- mk_dockerfile.sh
+++ mk_dockerfile.sh
@@ -9,10 +9,16 @@ if [ -z ${LEVEL+x} ]; then
 	LEVEL=scan-build
 fi
 
+if [ -z ${USE_TPCDS+x} ]; then
+	USE_TPCDS=0
+fi
+
 echo PG_VERSION=${PG_VERSION}
 echo LEVEL=${LEVEL}
+echo USE_TPCDS=${USE_TPCDS}
 
 sed \
 	-e 's/${PG_VERSION}/'${PG_VERSION}/g \
 	-e 's/${LEVEL}/'${LEVEL}/g \
+	-e 's/${USE_TPCDS}/'${USE_TPCDS}/g \
 	Dockerfile.tmpl > Dockerfile
