b492d62045866fff5e0bd1c317f84929c8453d33
Alexey Kondratov <kondratov.aleksey@gmail.com>
2021-05-11 20:18:34 +0300

    Fix TPC-DS tests, i.e. test data and schema generation

--- tests/prepare_stress.sh
+++ tests/prepare_stress.sh
@@ -4,23 +4,26 @@ mkdir -p tmp_stress
 cd tmp_stress
 rm -rf ./*
 
-git clone --depth 1 --single-branch --branch master https://github.com/gregrahn/tpcds-kit.git
-git clone --depth 1 --single-branch --branch master https://github.com/cwida/tpcds-result-reproduction.git
+git clone --depth 1 --single-branch --branch master https://github.com/gregrahn/tpcds-kit.git # used for data and schema
+git clone --depth 1 --single-branch --branch master https://github.com/cwida/tpcds-result-reproduction.git # used for queries only
 
 cd tpcds-kit/tools
-make -s
 
-#Generate data
+# This is a palliative care, since tpcds-kit is old and doesn't compile with modern ld.
+# Anyway, now it works and this is better than nothing.
+make LDFLAGS=-zmuldefs -s
+
+# Generate data
 ./dsdgen -FORCE -VERBOSE -SCALE 1
 
-#Prepare data
+# Prepare data
 mkdir -p tables
 for i in `ls *.dat`; do
   echo "Preparing file" $i
   sed 's/|$//' $i > tables/$i
 done
 
-#Generate queries
+# Generate queries
 ./dsqgen -DIRECTORY ../query_templates \
          -INPUT ../query_templates/templates.lst \
          -VERBOSE Y \
